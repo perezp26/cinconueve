e1b7f27f333e1ae3b5856f62da66de858dda6a06
Raul Perez <perezp@hotmail.com>
2023-10-20 18:31:48 -0600

    logo gris incluido

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,15 +9,19 @@ const [today, settoday] = useState(new Date())
   
 
   return (
-    <footer className={ `${styles.footer} ` }> 
+    <footer className={ `${styles.footer}` }> 
         <div className='md:flex'>
-            <div className='md:w-1/3'></div>
-            <div className='md:w-1/3 text-center'>
-                    <p className=' font-extralight text-2xl py-1'>Aviso de Privacidad</p>
-                    <p className=' font-extralight text-2xl py-1'>Términos y condiciones</p>
-                    <p className=' font-extralight text-2xl py-1'>cinconueve { today.getFullYear() } derechos reservados </p>
+            <div className='md:w-1/3'>
+                    <Image src='/imgs/logoCincoNueveGirs.png' width={ 158 }  height={ 144 } alt="logoCincoNueve" className=' mx-auto ' />
             </div>
-            <div className='md:w-1/3  flex md:mt-0 mt-5'>
+            <div className='md:w-1/3 text-center text-xl flex'>
+                    <div className=' m-auto'>
+                        <p className=' font-extralight py-1'>Aviso de Privacidad</p>
+                        <p className=' font-extralight py-1'>Términos y condiciones</p>
+                        <p className=' font-extralight py-1'>cinconueve { today.getFullYear() } derechos reservados </p>
+                    </div>
+            </div>
+            <div className='md:w-1/3  flex md:my-0 my-5'>
                   <div className=' w-full flex  m-auto '>
                       <div className='w-1/3 '>
                               <Image src='/imgs/whatsapp.png' width={ 45 }  height={ 45 } alt="facebook" className=' mx-auto ' />
